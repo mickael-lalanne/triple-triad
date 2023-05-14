acaa327f2b1aee6657cceacf81ce34b69f310f7e
Mickaël Lalanne <mickael.lalanne03@gmail.com>
2023-05-14 11:00:13 +0200

    Init login form with AWS Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "tripletriad695ad78b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- client/src/main.ts
+++ client/src/main.ts
@@ -3,10 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import awsExports from './aws-exports';
 
 Amplify.configure(awsExports);
+Auth.configure(awsExports);
 
 const app = createApp(App);
 
